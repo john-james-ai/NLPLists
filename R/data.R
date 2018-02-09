efa7604eda27faefd5dceeb340b53dddea87b13e
DataScienceSalon <datasciencesalon@gmail.com>
2018-02-08 22:04:45 -0500

    Documentation

---
+++ R/data.R
@@ -0,0 +1,43 @@
+#' Contractions.
+#'
+#' American English language contractions and their long forms.
+#'
+#' @format A data frame with two variables:
+#' \describe{
+#' \item{\code{key}}{Character string containing the contractions.}
+#' \item{\code{value}}{Character string containing the long form of the contraction.}
+#' }
+"contractions"
+
+#' Emoticons.
+#'
+#' Common emoticons used on the web and in messaging applications.
+#'
+#' @format A data frame with 1 variable:
+#' \describe{
+#' \item{\code{key}}{Character string containing the symbols that make up the emoticon.}
+#' }
+"emoticons"
+
+#' Internet Abbreviations.
+#'
+#' Common abbreviations used on the internet and in messaging platforms.
+#'
+#' @format A data frame with two variables:
+#' \describe{
+#' \item{\code{key}}{Character string containing the abbreviated text.}
+#' \item{\code{value}}{Character string containing the long form of the abbreviation.}
+#' }
+#'
+"internetAbbreviations"
+
+#' Profanity
+#'
+#' A list of words considered offensive for internet use.
+#'
+#' @format A data frame:
+#' \describe{
+#' \item{\code{key}}{Character string containing the offending text.}
+#' }
+#' @source \url{https://www.freewebheaders.com/full-list-of-bad-words-banned-by-google/}
+"profanity"
